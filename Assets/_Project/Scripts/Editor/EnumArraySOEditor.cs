856191d809af26e55dd94db864165f5e8590c6fd
Данила <106236167+Danila990@users.noreply.github.com>
2025-02-08 15:14:58 +0300

    path 0.0.1
    Улучшил кастомный редактор уровня

--- Assets/_Project/Scripts/Editor/EnumArraySOEditor.cs
+++ Assets/_Project/Scripts/Editor/EnumArraySOEditor.cs
@@ -16,47 +16,121 @@ namespace MyCode
             _gridSetting = (PlatfromGridSetting)target;
             base.OnInspectorGUI();
             LabelFieldText();
-            InitGrid();
-            CastomArray();
 
+            InitGrid();
+            CustomArray();
+            EditorGUILayout.Space(5);
+            LabelFieldTextSizeGrid();
+
+            EditorGUILayout.Space(5);
             EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.BeginVertical();
             AddLineButton();
+            RemoveLineButton();
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.BeginVertical();
             AddRowButton();
+            RemoveRowButton();
+            EditorGUILayout.EndHorizontal();
             ResetButton();
             EditorGUILayout.EndHorizontal();
 
             if (GUI.changed)
-            {
                 EditorUtility.SetDirty(target);
-            }
         }
 
-        private void CastomArray()
+        private void CustomArray()
         {
             for (int i = 0; i < _gridSetting.GridLinesArray.Length; i++)
             {
+                EditorGUILayout.Space(5);
                 EditorGUILayout.BeginHorizontal();
                 if (_gridSetting.GridLinesArray[i] != null)
                 {
                     for (int j = 0; j < _gridSetting.GridLinesArray[i].Line.Length; j++)
                     {
-                        GUI.color = Color.red;
+                        Color enumColor = GetPlatfromColor(_gridSetting.GridLinesArray[i].Line[j]);
+                        Rect rect = GUILayoutUtility.GetRect(20, 20); 
+                        EditorGUI.DrawRect(rect, enumColor);
+                        GUI.color = Color.white;
+
                         _gridSetting.GridLinesArray[i].Line[j] = (PlatformType)EditorGUILayout.EnumPopup(_gridSetting.GridLinesArray[i].Line[j]);
                         GUI.color = Color.white;
                     }
                 }
+
                 EditorGUILayout.EndHorizontal();
             }
+        }
+
+        private Color GetPlatfromColor(PlatformType platformType)
+        {
+            Color color;
+            switch (platformType)
+            {
+                case PlatformType.Default:
+                    color = Color.white;
+                    break;
+
+                case PlatformType.Player:
+                    color = Color.green;
+                    break;
+
+                case PlatformType.Empty:
+                    color = Color.black;
+                    break;
+
+                default:
+                    color = Color.white;
+                    break;
+            }
+
+            return color;
+        }
+
+        private void LabelFieldTextSizeGrid()
+        {
+            Vector2Int sizeGrid = new Vector2Int(_gridSetting.GridLinesArray.Length, _gridSetting.GridLinesArray[0].Line.Length);
+            EditorGUILayout.LabelField($"Size Grid: X - {sizeGrid.x}, Y - {sizeGrid.y}");
         }
 
         private void AddLineButton()
         {
             if (GUILayout.Button("Add Line"))
             {
-                GridLine[] newGridLine = new GridLine[_gridSetting.GridLinesArray.Length + 1];
-                _gridSetting.GridLinesArray.CopyTo(newGridLine, 0);
-                newGridLine[_gridSetting.GridLinesArray.Length] = new GridLine() { Line = new PlatformType[_gridSetting.GridLinesArray[0].Line.Length] };
-                _gridSetting.GridLinesArray = newGridLine;
+                GridLine[] newGridLines = new GridLine[_gridSetting.GridLinesArray.Length + 1];
+                _gridSetting.GridLinesArray.CopyTo(newGridLines, 0);
+                newGridLines[_gridSetting.GridLinesArray.Length] = new GridLine() { Line = new PlatformType[_gridSetting.GridLinesArray[0].Line.Length] };
+                _gridSetting.GridLinesArray = newGridLines;
+            }
+        }
+
+        private void RemoveLineButton()
+        {
+            if (GUILayout.Button("Remove Line"))
+            {
+                if (_gridSetting.GridLinesArray.Length <= 1)
+                    return;
+
+                GridLine[] newGridLines = new GridLine[_gridSetting.GridLinesArray.Length - 1];
+                Array.Copy(_gridSetting.GridLinesArray, newGridLines, newGridLines.Length);
+                _gridSetting.GridLinesArray = newGridLines;
+            }
+        }
+
+        private void RemoveRowButton()
+        {
+            if (GUILayout.Button("Remove Row"))
+            {
+                if(_gridSetting.GridLinesArray[0].Line.Length <= 1)
+                    return;
+
+                for (int i = 0; i < _gridSetting.GridLinesArray.Length; i++)
+                {
+                    PlatformType[] newRow = new PlatformType[_gridSetting.GridLinesArray[i].Line.Length -1];
+                    Array.Copy(_gridSetting.GridLinesArray[i].Line, newRow, newRow.Length);
+                    _gridSetting.GridLinesArray[i].Line = newRow;
+                }
             }
         }
 
